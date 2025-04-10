626c6bfbe3dc362b13bf275196eadae628c3ddd2
adhish1478 <adhisharavind@Adhishs-MacBook-Air.local>
2025-04-10 16:29:01 +0530

    login, profile completed, logout did not

--- login/urls.py
+++ login/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('login/', views.login, name='login'),
-    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('profile/', views.profile, name='profile')
 
 ]
--- login/views.py
+++ login/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 
@@ -7,10 +7,27 @@ from django.contrib import messages
 def homepage(request):
     return render(request, 'home.html')
 
-def login(request):
-    return render(request, 'login.html')
+def profile(request):
+    return render(request, 'profile.html')
 
-def register(request):
+def login_view(request):
+    if request.method == 'POST':
+        username =request.POST['username']
+        password= request.POST['password']
+
+        user= auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('profile')
+        
+        else:
+            messages.info(request, 'Invalid credentials')
+            return render(request, 'login.html')
+    else:
+        return render(request, 'login.html')
+        
+
+def register_view(request):
     if request.method == 'POST':
         username = request.POST['username']
         password1 = request.POST['password1']
@@ -19,20 +36,18 @@ def register(request):
         
         if password1 == password2:
             if User.objects.filter(username=username).exists():
-                message='Username already exists'
                 messages.info(request, 'Username already exists')
                 return render(request, 'register.html')
             
             elif User.objects.filter(email=email).exists():
                 messages.info(request, 'Email already exists')
-                message='Email already exists'
                 return render(request, 'register.html')
             
             else:
                 user= User.objects.create_user(username=username, password=password1, email=email)
                 user.save()
                 print('User created')
-                return render(request, 'login.html')
+                return redirect('login')
         else:
             print('password not matching')
             messages.info(request, 'Password not matching')
