1be6e09668fab67197aaf7f298b318e3f94df01a
adhish1478 <adhisharavind@Adhishs-MacBook-Air.local>
2025-04-10 15:23:38 +0530

    register edge case

--- login/views.py
+++ login/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 
 # Create your views here.
@@ -12,13 +13,30 @@ def login(request):
 def register(request):
     if request.method == 'POST':
         username = request.POST['username']
-        password = request.POST['password']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
         email = request.POST['email']
         
-        user= User.objects.create_user(username=username, password=password, email=email)
-        user.save()
-        print('User created')
-        return render(request, 'login.html')
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                message='Username already exists'
+                messages.info(request, 'Username already exists')
+                return render(request, 'register.html')
+            
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already exists')
+                message='Email already exists'
+                return render(request, 'register.html')
+            
+            else:
+                user= User.objects.create_user(username=username, password=password1, email=email)
+                user.save()
+                print('User created')
+                return render(request, 'login.html')
+        else:
+            print('password not matching')
+            messages.info(request, 'Password not matching')
+            return render(request, 'register.html')
 
     else:
         return render(request, 'register.html')
