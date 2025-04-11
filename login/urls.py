c0fab1bd47b0b9d68511a291b7d66fbaaf6e46f2
adhish1478 <adhisharavind@Adhishs-MacBook-Air.local>
2025-04-11 17:18:08 +0530

    all added without proper security

--- login/urls.py
+++ login/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
-    path('profile/', views.profile, name='profile')
+    path('profile/', views.profile, name='profile'),
+    path('logout/', views.logout_view, name='logout'),
 
 ]
--- login/views.py
+++ login/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 
 
 # Create your views here.
 def homepage(request):
     return render(request, 'home.html')
 
+@login_required(login_url='login')
 def profile(request):
     return render(request, 'profile.html')
 
@@ -56,3 +59,7 @@ def register_view(request):
     else:
         return render(request, 'register.html')
 
+def logout_view(request):
+    auth.logout(request)
+    messages.success(request, 'You have been logged out successfully.')
+    return redirect('/')
